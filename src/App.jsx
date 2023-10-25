0b5ca42d954e503298270b2e81ffba23564d593f
mohamedeld <mohamed.azoz20010@gmail.com>
2023-10-25 12:15:34 +0300

    add table and validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-
-import './App.css'
+import { useState } from 'react'
 import Header from './components/Header'
+import Results from './components/Results'
 import UserInput from './components/UserInput'
+import './App.css'
 
 function App() {
-  
+  const [userInput,setUserInput] = useState({
+    initialInvestment:10000,
+    annualInvestment:1200,
+    expectedReturn:6,
+    duration:10
+});
 
+function handleChange(inputIdentifier,newValue){
+    setUserInput(prevState=>{
+        return {
+            ...prevState,
+            [inputIdentifier]:+newValue
+        }
+    })
+}
+  const validInput = userInput.duration >= 1;
   return (
     <>
      <Header/>
-     <UserInput/>
+     <UserInput userInput={userInput} onHandleChange={handleChange}/>
+     {!validInput && <p className='center'>duration must be greater than 0</p>}
+     { validInput && <Results userInput={userInput}/>}
     </>
   )
 }
